65cf3cba78bc9d2597c646904145ecb2df853d9b
farrukh135 <afarrukh302@gmail.com>
2024-05-15 17:42:06 +0500

    Create First Component

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>Chai aur React | Farrukh Ali</h2>
+      <h3>
+        It's an extraordinary series, where I learned how to initialize a React
+        project, how to get started, understand packages, dependencies, and much
+        more about starting my project. Where to initiate it from, what things
+        need to be deleted, and how to manipulate it.
+      </h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>Chai aur React with Vite | Farrukh Ali</h2>
+      <h3>
+        It is an extraordinary series, where I learned how to initialize a React
+        project, how to get started, understand packages, dependencies, and much
+        more about starting my project. Where to initiate it from, what things
+        need to be deleted, and how to manipulate it.
+      </h3>
+    </>
+  );
+}
+
+export default App;
