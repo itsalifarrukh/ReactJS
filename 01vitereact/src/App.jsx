e7615e9a8f7e8b254a08734fd14803e3f318b27d
farrukh135 <afarrukh302@gmail.com>
2024-05-15 19:59:51 +0500

    Create your own React Library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Chai from "./Chai";
 function App() {
+  const username = "Farrukh Ali";
   return (
     <>
       <h1>Hello World!</h1>
-      <h2>Chai aur React with Vite | Farrukh Ali</h2>
+      <h2>Chai aur React with Vite | {username} </h2>
       <h3>
         It is an extraordinary series, where I learned how to initialize a React
         project, how to get started, understand packages, dependencies, and much
